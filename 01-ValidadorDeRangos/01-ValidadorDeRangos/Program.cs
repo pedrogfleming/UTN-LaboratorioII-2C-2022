23766ecdbf244ec49ea09a952b03c54a4880d29c
Pedro Fleming <pedro.fleming@globallogic.com>
2022-08-25 20:59:46 -0300

    I01

---
+++ 01-ValidadorDeRangos/01-ValidadorDeRangos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace _01_ValidadorDeRangos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumeros = 0;
+            //Declaro un array y lo inicializo con un largo de 10 elementos
+            int[] numeros = new int[10];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Ingrese un  número");
+                string input = Console.ReadLine();
+                int numeroIngresado = 0;
+                bool esEntero = int.TryParse(input, out numeroIngresado);
+                while (!esEntero || !Validador.Validar(numeroIngresado,-100,100))
+                {
+                    Console.WriteLine("Error, ingrese unicamente un número entre -100 y 100");
+                    input = Console.ReadLine();
+                    esEntero = int.TryParse(input, out numeroIngresado);
+                }
+                numeros[i] = (numeroIngresado);
+                totalNumeros++;
+            }
+            Array.ForEach(
+                numeros,n =>
+                    Console.WriteLine($"El número en la posición {numeros.GetEnumerator} es {n}"));
+            Console.WriteLine($"El número más grande es {numeros.Max()}");
+            Console.WriteLine($"El número más chico es {numeros.Min()}");
+            Console.WriteLine($"El promedio de los números ingresados es {numeros.Average()}");
+        }
+    }
+}
