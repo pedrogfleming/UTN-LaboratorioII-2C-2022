1012e6006b15a95dc1448ae2dc37ee7839869e37
Pedro Fleming <pedro.fleming@globallogic.com>
2022-09-01 20:13:50 -0300

    ejemplo de propiedades y atributos

--- 02-Datetimes/I02-VosCuantasPrimaverasTenes/EjPropiedades_Atributos/Auto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace I02_VosCuantasPrimaverasTenes
-{
-    public class Auto
-    {
-        public int peso;
-        public int Peso { get; set; }
-    }
-}
---
+++ 02-EjemploPropiedadesYAtributos/EjPropiedades/EjPropiedades/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EjPropiedades
+{
+    internal class Program
+    {
+        public class Auto
+        {
+            public int peso;
+            public int Peso { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Auto a1 = new Auto() { peso = 45 };
+            Console.WriteLine(a1.peso);
+            a1.Peso = 2;
+            Console.WriteLine(a1.Peso); ;
+        }
+    }
+}
