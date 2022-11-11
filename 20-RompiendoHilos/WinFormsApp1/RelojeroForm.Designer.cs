f939e2d53dd901db98df76ea2961b987e3c412f8
Pedro Fleming <pedro.fleming@globallogic.com>
2022-11-10 21:28:38 -0300

    Reloj ej resuelto

---
+++ 20-RompiendoHilos/WinFormsApp1/RelojeroForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WinFormsApp1
+{
+    partial class RelojeroForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHora = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(87, 44);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(59, 25);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 184);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblHora);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ 20-RompiendoHilos/WinFormsApp1/RelojeroForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class RelojeroForm : Form
+    {
+        public RelojeroForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                AsignarHora();
+            });
+        }
+        public void AsignarHora()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                if (this.lblHora.InvokeRequired)
+                {
+                    #region Usando MethoInoker
+                    //this.lblHora.BeginInvoke((MethodInvoker)delegate ()
+                    //{
+                    //    this.lblHora.Text = DateTime.UtcNow.ToString();
+                    //});
+                    #endregion
+                    #region Usando Action
+                    Action ac = new Action(() =>
+                    {
+                        this.lblHora.Text = DateTime.UtcNow.ToString();
+                    });
+
+                    var result = this.lblHora.BeginInvoke(ac);
+                    #endregion
+                }
+                else
+                {
+                    this.lblHora.Text = DateTime.UtcNow.ToString();
+                }
+            }
+        }
+    }
+}
