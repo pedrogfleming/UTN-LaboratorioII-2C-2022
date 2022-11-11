f023c409ff3d2bfbc786e0e5bad06049d2acd147
Pedro Fleming <pedro.fleming@globallogic.com>
2022-11-10 21:39:21 -0300

    WaitAll Arreglado

--- 20-RompiendoHilos/20-RompiendoHilos/Program.cs
+++ 20-RompiendoHilos/20-RompiendoHilos/Program.cs
@@ -10,59 +10,74 @@ namespace _20_RompiendoHilos
         static void Main(string[] args)
         {
             #region Ejemplo 1
-            //Task t1 = new Task(() =>
-            //{
-            //    try
-            //    {
-            //        throw new Exception("Desde el hilo 1 tiro la excepcion");
-            //    }
-            //    catch (Exception)
-            //    {
-            //        Console.WriteLine("Desde adentro del catch 1");
-            //    }
-            //});
-            //Task t2 = new Task(() =>
-            //{
-            //    throw new Exception("Desde el hilo 2 tiro la excepcion");
-            //    Console.WriteLine("Desde adentro del try 2");
-            //});
-
-            //t1.Start();
-            //t2.Start();
-            //Console.WriteLine("Fin del programa");
-            #endregion
-            CancellationTokenSource tokenSource = new CancellationTokenSource();
-            CancellationToken token = tokenSource.Token;
-            int i = 0;
             Task t1 = new Task(() =>
             {
-                
-                while (!tokenSource.IsCancellationRequested)
+                try
                 {
-                    Console.WriteLine($"T1 - iteracion {i} dentro del t1");
-                    i++;
+                    throw new Exception("Desde el hilo 1 tiro la excepcion");
                 }
-            }, token);
+                catch (Exception)
+                {
+                    Console.WriteLine("Desde adentro del catch 1");
+                }
+            });
             Task t2 = new Task(() =>
             {
-                while (i < 10)
+                try
                 {
-                    Console.WriteLine($"T2 - Todavia no puedo cancelar t1, i= {i}");
+                    throw new Exception("Desde el hilo 2 tiro la excepcion");
                 }
-                Console.WriteLine($"Se cancela T1, i = {i}");
-                tokenSource.Cancel();
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Desde adentro del catch 2");
+                }
             });
 
-            List<Task> tasks = new();
+            List<Task> tasks = new List<Task>();
             tasks.Add(t1);
             tasks.Add(t2);
-            foreach (Task t in tasks)
-            {
-                t.Start();
-            }
-            
+            t1.Start();
+            t2.Start();
             Task.WaitAll(tasks.ToArray());
             Console.WriteLine("Fin del programa");
+            #endregion
+
+            #region Ejemplo CancellToken 
+            //CancellationTokenSource tokenSource = new CancellationTokenSource();
+            //CancellationToken token = tokenSource.Token;
+            //int i = 0;
+            //Task t1 = new Task(() =>
+            //{
+
+            //    while (!tokenSource.IsCancellationRequested)
+            //    {
+            //        Console.WriteLine($"T1 - iteracion {i} dentro del t1");
+            //        i++;
+            //    }
+            //}, token);
+            //Task t2 = new Task(() =>
+            //{
+            //    while (i < 10)
+            //    {
+            //        Console.WriteLine($"T2 - Todavia no puedo cancelar t1, i= {i}");
+            //    }
+            //    Console.WriteLine($"Se cancela T1, i = {i}");
+            //    tokenSource.Cancel();
+            //});
+
+            //List<Task> tasks = new();
+            //tasks.Add(t1);
+            //tasks.Add(t2);
+            //foreach (Task t in tasks)
+            //{
+            //    t.Start();
+            //}
+
+            //Task.WaitAll(tasks.ToArray());
+
+            #endregion
+            
         }
     }
 }
